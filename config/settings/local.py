9529de2c4b48e018cb94dac12b1513568fb5c3eb
markmur90 <markmur90@proton.me>
2025-05-29 23:09:10 +0200

    BASE

---
+++ config/settings/local.py
@@ -0,0 +1,18 @@
+# from config.settings.configuración_dinamica import OAUTH2
+from .base1 import *
+
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Configuraciones específicas del entorno local
+USE_OAUTH2_UI = False
+
+# REDIRECT_URI = os.getenv("REDIRECT_URI", "http://0.0.0.0:8000/oauth2/callback/")
+# ORIGIN = os.getenv("ORIGIN", "http://0.0.0.0:8000")
+
+# OAUTH2.update({
+#     "REDIRECT_URI": REDIRECT_URI,
+#     "ORIGIN": ORIGIN,
+# })
