40baa0df7a4db83afddcc5ebddacccc9ea64f646
markmur90 <markmur90@proton.me>
2025-05-29 11:45:32 +0200

    delete

--- api/context_processors.py
+++
@@ -1,5 +0,0 @@
-def entorno_actual(request):
-    return {
-        'entornos': ['local', 'sandbox', 'produccion'],
-        'entorno_actual': request.session.get('entorno_actual', 'produccion')
-    }
