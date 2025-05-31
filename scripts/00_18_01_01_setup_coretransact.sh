a8d5e71c13881e73a9790a63959aaffefd4700a7
markmur90 <markmur90@proton.me>
2025-05-31 09:12:43 +0200

    deploy5

--- scripts/00_18_01_01_setup_coretransact.sh
+++ scripts/00_18_01_01_setup_coretransact.sh
@@ -36,45 +36,43 @@ apt-get update && apt-get full-upgrade -y && apt-get autoremove -y
 apt install -y git curl build-essential ufw fail2ban python3 python3-pip python3-venv python3-dev libpq-dev postgresql postgresql-contrib nginx certbot python3-certbot-nginx supervisor
 
 echo "🧱 Activando firewall UFW..."
-sudo ufw default allow incoming
+
+# Políticas seguras por defecto
+sudo ufw default deny incoming
 sudo ufw default allow outgoing
-# Reglas básicas
-sudo ufw allow 22/tcp      # SSH
+
+# Accesos remotos permitidos
+sudo ufw allow 49222/tcp   # Puerto SSH personalizado
+sudo ufw allow 22/tcp      # SSH fallback (si aún se usa)
 sudo ufw allow 80/tcp      # HTTP
-sudo ufw allow 8000/tcp    # HTTPS
-# sudo ufw allow 18080/tcp    # HTTPS
-# sudo ufw allow 18081/tcp    # HTTPS
-# sudo ufw allow 28080/tcp    # HTTPS
-# sudo ufw allow 28081/tcp    # HTTPS
-sudo ufw allow 49222/tcp   # HTTPS NJALLA
-sudo ufw allow out to any port 443 #PUSH
-# Gunicorn y PostgreSQL solo local
+sudo ufw allow 443/tcp     # HTTPS
+
+# Servicios internos / loopback
 sudo ufw allow from 127.0.0.1 to any port 8000
 sudo ufw allow from 127.0.0.1 to any port 8011
 sudo ufw allow from 127.0.0.1 to any port 8001
 sudo ufw allow from 127.0.0.1 to any port 5432
+
 # Honeypot SSH
-# sudo ufw allow 2222/tcp
+sudo ufw allow 2222/tcp
+
 # Supervisor local
 sudo ufw allow from 127.0.0.1 to any port 9001
+
 # Tor
 sudo ufw allow from 127.0.0.1 to any port 9050
 sudo ufw allow from 127.0.0.1 to any port 9051
-# # DNS y NTP salientes
+
+# Salida DNS, NTP, Push
 sudo ufw allow out 53
 sudo ufw allow out 123/udp
-# # Heroku CLI saliente
-# sudo ufw allow out to any port 443
-# Monero (XMR)
-# sudo ufw allow 18080/tcp                                     # Nodo P2P abierto
-# sudo ufw allow proto tcp from 127.0.0.1 to any port 18082    # Wallet RPC local
-# sudo ufw allow proto tcp from 127.0.0.1 to any port 18089:18100  # Rango wallets
-# Livereload (local)
-# sudo ufw allow from 127.0.0.1 to any port 35729
-# Ghost API (local)
-# sudo ufw allow from 127.0.0.1 to any port 5000
-# Activar UFW
+sudo ufw allow out to any port 443 proto tcp
+
+# Activación
 sudo ufw --force enable
+sudo ufw reload
+
+
 
 echo "🎯 Hostname y zona horaria..."
 hostnamectl set-hostname coretransapi
