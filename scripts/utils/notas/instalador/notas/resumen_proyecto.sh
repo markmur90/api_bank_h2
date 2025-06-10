9f0750a587f3c12565fb5503f059200d7c487bc4
markmur90 <markmur90@proton.me>
2025-06-10 08:18:45 +0200

    delete

--- scripts/utils/notas/instalador/notas/resumen_proyecto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "📦 Resumen total del proyecto"
-echo "📝 Notas acumuladas:"
-find "$HOME/notas/texto" -type f -name "*.txt" -exec echo "🗒 {}" \; -exec cat {} \;
-echo -e "\n🎤 Audios acumulados:"
-find "$HOME/notas/audio" -type f -name "voz_*.wav"
