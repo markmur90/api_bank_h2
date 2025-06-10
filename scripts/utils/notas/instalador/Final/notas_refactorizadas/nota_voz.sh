5f4a1de8f1e7f580217876deef0317541e1ea03b
markmur90 <markmur90@proton.me>
2025-06-10 08:22:22 +0200

    delete

--- scripts/utils/notas/instalador/Final/notas_refactorizadas/nota_voz.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FECHA=$(date '+%Y-%m-%d')
-HORA=$(date '+%H-%M-%S')
-DIR="$HOME/notas/audio/$FECHA"
-mkdir -p "$DIR"
-FILENAME="voz_$HORA.wav"
-echo "🎙 Grabando 60s... (Ctrl+C para cortar antes)"
-arecord -d 60 -f cd -t wav "$DIR/$FILENAME"
-echo "✅ Audio guardado en $DIR/$FILENAME"
