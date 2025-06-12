890f8a6166df561cd224b804fa548f04a1d2fdf2
markmur90 <markmur90@proton.me>
2025-06-12 03:22:48 +0200

    gitignore

--- scripts/utils/notas/aliases_deploy.sh
+++
@@ -1,6 +0,0 @@
-# Ruta del script de notificaciones
-NOTIF_DIR="$UTILS_DIR/notificaciones"
-TG_CONF_SCRIPT="$NOTIF_DIR/telegram_config.sh"
-
-# Alias para configurar Telegram
-alias tg_bot_conf='bash "$TG_CONF_SCRIPT"'
