d859c0cf588592cec8eccbb2bb373c06c4cae5af
markmur90 <markmur90@proton.me>
2025-06-28 22:54:07 +0200

    Cambios a ser confirmados: 	modificados:     api/urls.py 	modificados:     config/settings/base1.py 	modificados:     config/urls.py

--- api/urls.py
+++ api/urls.py
@@ -9,17 +9,8 @@ from api.views import (
 urlpatterns = [
     # Asegúrate de que solo haya un namespace 'admin'
     path('', HomeView.as_view(), name='home'),
-    # path('login/', LoginView.as_view(), name='login'),
-    # path('logout/', LogoutView.as_view(), name='logout'),
-    # path('app/api/auth/index.html', AuthIndexView.as_view(), name='auth_index'),
     path('app/core/index.html', CoreIndexView.as_view(), name='core_index'),
-    # path('app/accounts/index.html', AccountsIndexView.as_view(), name='accounts_index'),
-    # path('app/transactions/index.html', TransactionsIndexView.as_view(), name='transactions_index'),
-    path('app/transfers/index.html', TransfersIndexView.as_view(), name='transfers_index'),
-    # path('app/collection/index.html', CollectionIndexView.as_view(), name='collection_index'),
-    # path('app/sct/index.html', SCTIndexView.as_view(), name='sct_index'),
 
-    # path("readme/", ReadmeView.as_view(), name="readme_deploy"),
     path("readme/", mostrar_readme, name="readme_deploy"),
 
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
